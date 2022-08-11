1243d8e18bf0c7bdf24291cc5efc85edec901602
claudio duchi <claduc@gmail.com>
2022-08-11 11:37:18 +0200

    aggiunto somma per differenza. Creato malgebretta separando pgfkey.
    Creato template per inserimento in dtx

--- compila.bat
+++ compila.bat
@@ -1,3 +1,26 @@
+del *.idx
+del *.glo
+del *.aux
+del *.gls
+del *.ind
+del *.log
+del *.gz
+del *.hd
+del *.ins
+pdflatex -shell-escape malgebretta.dtx
+makeindex -s gind.ist malgebretta
+makeindex -s gglo.ist malgebretta.glo -o malgebretta.gls
+pdflatex -shell-escape malgebretta.dtx
+makeindex -s gind.ist malgebretta
+makeindex -s gglo.ist malgebretta.glo -o malgebretta.gls
+pdflatex -shell-escape malgebretta.dtx
+makeindex -s gind.ist malgebretta
+makeindex -s gglo.ist malgebretta.glo -o malgebretta.gls
+pdflatex -shell-escape malgebretta.dtx
+makeindex -s gind.ist malgebretta
+makeindex -s gglo.ist malgebretta.glo -o malgebretta.gls
+pdflatex -shell-escape malgebretta.dtx
+
 pdflatex -shell-escape algebretta.dtx
 makeindex -s gind.ist algebretta
 makeindex -s gglo.ist algebretta.glo -o algebretta.gls
